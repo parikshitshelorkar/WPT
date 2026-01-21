3d9f1c199136fa247c5b0dd3a48c15e3b325e159
parikshitshelorkar <parikshitshelorkar2.0@gmail.com>
2026-01-21 20:58:10 +0530

    adding files

---
+++ Hello/hello.js
@@ -0,0 +1,40 @@
+var company = "Transflower";
+var status = false;
+var count = 67;
+
+console.log("Hello World !");
+console.log(company);
+console.log(status);
+console.log(count);
+
+//JSON -> Javascript Object Notation
+var flower = {
+    id : 45,
+    title : "Gerbera",
+    description: "Wedding flower",
+    unitdescription: 12,
+    quantity: 56000,
+    likes: 12000,
+}
+
+console.log(flower.title +" "+ flower.description);
+
+
+var flowers = []; //empty Array
+var freshFlower=["Rose", "Gerbera", "jasmine"]; //array of strings
+var marks=[23,54, 67, 23]; //array of numbers
+
+flowers=[
+    {id : 41,title : "Gerbera",description: "Wedding flower",unitdescription: 10,quantity: 560,likes: 12000},
+    {id : 42,title : "Rose",description: "Valentine flower",unitdescription: 111,quantity: 9000,likes: 12000},
+    {id : 43,title : "Jasmine",description: "Aromatic flower",unitdescription: 122,quantity: 6000,likes: 12000},
+    {id : 44,title : "Lily",description: "Easter flower",unitdescription: 10,quantity: 90,likes: 12000},
+    {id : 45,title : "Tulip",description: "Spring flower",unitdescription: 2,quantity: 7000,likes: 12000},
+    {id : 46,title : "Orchid",description: "Exotic flower",unitdescription: 21,quantity: 56000,likes: 12000},
+    { id : 47,title : "Daffodil",description: "Cheerful flower",unitdescription: 11,quantity: 56000,likes: 12000},
+    {id : 48,title : "Sunflower",description: "Summer flower",unitdescription: 12,quantity: 56000,likes: 12000},
+    {id : 49,title : "Carnation",description: "Mother's day flower",unitdescription: 12,quantity: 56000,likes: 12000}
+];
+
+
+
---
+++ Hello/timer.js
@@ -0,0 +1,29 @@
+
+function show(){
+    var count=67;
+    count++;
+    console.log("Count is: "+count);
+};
+
+function display(company){
+    console.log("Company is: "+company);
+};
+
+function addition(num1, num2){
+var sum=num1+num2;
+return sum;
+};
+
+
+show(); //invoking js function
+
+var provider = "Transflower Learning Pvt Ltd";
+var hostCompany = "Seed Infotech Ltd.";
+
+display(hostCompany); //function with parameter
+
+var number1 = 45;
+var number2 = 55;
+var result = addition(number1, number2);
+
+console.log("Final output = "+ total);
