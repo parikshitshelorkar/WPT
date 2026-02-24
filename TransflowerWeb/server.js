a71cd23f1703ede04002b0ffb0b8faba917c8b50
parikshitshelorkar <parikshitshelorkar2.0@gmail.com>
2026-02-24 18:44:23 +0530

    adding new WebApps

--- TransflowerWeb/server.js
+++ TransflowerWeb/server.js
@@ -8,30 +8,21 @@ var staticMiddleware = express.static(path.join(__dirname, "public"));
 //Express framework configuration
 app.use(staticMiddleware);
 
-//Express framework setting handlers
-app.get("/index", (request, response) => {
+//Express framework setting handlers 
+
+app.get("/", (request, response) => {
     response.sendFile(__dirname + "/index.html");
 });
 
-app.get("/about", (request, response) => {
-    response.sendFile(__dirname + "/about.html");
-});
 
 app.listen(9000);
 console.log("Express web app is listening on port 9000");
 
-app.get("/contact", (request, response) => {
-    response.sendFile(__dirname + "/contact.html");
+app.post("login", (request, response) => {
+  console.log("Login form is posted by browser is received by server");
 });
 
-app.get("/services", (request, response) => {
-    response.sendFile(__dirname + "/services.html");
-}); 
-
-app.post("/login", (request, response) => {
-    response.sendFile(__dirname + "/login.html")
-});
-app.post("/register", (request, response)=>{
-    response.sendFile(__dirname, + "/register.html")
+app.post("register", (request, response)=>{
+  console.log("Register form is posted by broweser is received by server");
 });
 
